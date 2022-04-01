4eb88a46a0f6f221137cd7e57c678fe4ed2d6015
kobayashidm <kobayashidm@gmail.com>
2022-03-31 21:23:59 -0300

    exercicio 1

---
+++ exercicio1.ts
@@ -0,0 +1,22 @@
+const ingredientes = [
+  '2 hambúrgueres',
+  'alface',
+  'queijo',
+  'molho especial',
+  'cebola',
+  'picles',
+  'pão com gergelim'
+];
+
+
+const fazUmBigMac = (ingredientes: string[]): string => {
+  let bigMac : string = ingredientes.reduce(meuBigMac);
+  function meuBigMac(total :string , palavra: string)
+  {
+    return total+ ', '+ palavra;
+  }
+  console.log (bigMac);
+  return bigMac;
+};
+
+fazUmBigMac(ingredientes);
